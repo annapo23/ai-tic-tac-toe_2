43250a66561ba3da7b7bf0b3ff4585a9e14cf889
Anna Pojawis <annapojawis@gmail.com>
2020-02-09 20:16:43 -0800

    add server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+const port = 3000;
+
+app.use('/', express.static(path.join(__dirname, './public')));
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
